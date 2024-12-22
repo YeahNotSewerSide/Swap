1ad5531a61d646cb3c6d2922d4471826ac59084b
ArtemkaDev <mine38883@gmail.com>
2024-12-22 17:20:40 +0200

    adsasd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookie = headers().get("cookie");
-
   return (
     <html lang="en">
       <body className={inter.className}>
